cc339d3fe2c571f18fdc5823ed59e0ae13637b64
cassiaa <cassia.artanegara@gmail.com>
2019-04-28 19:01:17 -0700

    changed code to reuse old photo elements; about div still doesn't work

--- feed.js
+++ feed.js
@@ -1,5 +1,6 @@
 let current_photo = 0;      // Updated when user selects a new photo
 let num_photos = 8;         // Needs to be updated as new photos are added
+let photo_cache = ["<div id='gallery0'>Hello!</div>", null, null, null, null, null, null, null];   
 
 // direction == -1 --> back
 // direction == 1 --> forward
@@ -25,23 +26,44 @@ function arrowClick(direction) {
 }
 
 function updateViewer(index) {
+    console.log(photo_cache);
+    
+    let id_of_old_img = 'gallery' + current_photo;
+    // Hide the old element
+    document.getElementById(id_of_old_img).style.display = "none";
+    
+    // Set the current_photo var to the index of the new photo
     current_photo = index;
-    
-    console.log(index);
-    
-    let photo = document.getElementById('photo');
-    photo.innerHTML = "";
+    console.log("current_photo = " + current_photo);
 
-    // Prevents trying to find an image if user is looking at about div
-    if (index == 0) {
-        document.getElementById('aboutDiv').style.display = 'block';
+    if (photo_cache[current_photo] != null) {
+        document.getElementById('gallery' + current_photo).style.display = 'block';
     } else {
+        let photo = document.getElementById('photo');
         // Create a new image object from the index parameter
         let new_img = document.createElement("IMG");
         new_img.setAttribute('src', 'images/gallery/' + index + '.jpg');
+        new_img.setAttribute('id', 'gallery'+index);
         new_img.setAttribute('class', 'gallery');
-        photo.appendChild(new_img);    
+        photo_cache.splice(current_photo, 1, new_img);
+        photo.appendChild(new_img);  
     }
+
+    
+    
+//    photo.innerHTML = "";
+
+    // // Prevents trying to find an image if user is looking at about div
+    // if (index == 0) {
+    //     document.getElementById('0').style.display = 'block';
+    // } else {
+    //     // Create a new image object from the index parameter
+    //     let new_img = document.createElement("IMG");
+    //     new_img.setAttribute('src', 'images/gallery/' + index + '.jpg');
+    //     new_img.setAttribute('id', 'gallery'+index);
+    //     new_img.setAttribute('class', 'gallery');
+    //     photo.appendChild(new_img);    
+    // }
 
     
 
@@ -55,3 +77,4 @@ for (var i = 0; i < thumbnails.length; i++) {
         updateViewer(this.id);
     })
 }
+
